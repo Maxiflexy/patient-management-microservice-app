dbd405211337d61e019b55056ad120be7791a292
Maxiflexy <onyemax247@gmail.com>
2025-03-22 20:26:16 +0100

    added grpc client impl for patient-service

--- patient-service/src/main/java/com/maxiflexy/patientservice/service/PatientServiceImpl.java
+++ patient-service/src/main/java/com/maxiflexy/patientservice/service/PatientServiceImpl.java
@@ -4,6 +4,7 @@ import com.maxiflexy.patientservice.dto.request.PatientRequestDTO;
 import com.maxiflexy.patientservice.dto.response.PatientResponseDTO;
 import com.maxiflexy.patientservice.exception.EmailAlreadyExistsException;
 import com.maxiflexy.patientservice.exception.PatientNotFoundException;
+import com.maxiflexy.patientservice.grpc.BillingServiceGrpcClient;
 import com.maxiflexy.patientservice.mapper.PatientMapper;
 import com.maxiflexy.patientservice.model.Patient;
 import com.maxiflexy.patientservice.repository.PatientRepository;
@@ -19,6 +20,7 @@ import java.util.UUID;
 public class PatientServiceImpl implements PatientService{
 
     private final PatientRepository patientRepository;
+    private final BillingServiceGrpcClient billingServiceGrpcClient;
 
     @Override
     public List<PatientResponseDTO> getPatients(){
@@ -37,6 +39,9 @@ public class PatientServiceImpl implements PatientService{
             );
         }
         var newPatient = patientRepository.save(PatientMapper.toPatientModel(patientRequestDTO));
+
+        billingServiceGrpcClient.createBillingAccount(newPatient.getId().toString(),
+                newPatient.getName(), newPatient.getEmail());
 
         return PatientMapper.toDTO(newPatient);
     }
