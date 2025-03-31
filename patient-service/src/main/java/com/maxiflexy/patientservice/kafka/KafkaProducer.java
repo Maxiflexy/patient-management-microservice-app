bc2f50667213fa8eebb2c2e08deacb46489711be
Maxiflexy <onyemax247@gmail.com>
2025-03-31 12:02:32 +0100

    added proto file for kafka producer

--- patient-service/src/main/java/com/maxiflexy/patientservice/kafka/KafkaProducer.java
+++ patient-service/src/main/java/com/maxiflexy/patientservice/kafka/KafkaProducer.java
@@ -1,10 +1,29 @@
 package com.maxiflexy.patientservice.kafka;
 
+import com.maxiflexy.patientservice.model.Patient;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.core.KafkaTemplate;
+import patient.events.PatientEvent;
 
 @RequiredArgsConstructor
+@Slf4j
 public class KafkaProducer {
 
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
+
+    public void sendEvent(Patient patient) {
+        PatientEvent event = PatientEvent.newBuilder()
+                .setPatientId(patient.getId().toString())
+                .setName(patient.getName())
+                .setEmail(patient.getEmail())
+                .setEventType("PATIENT_CREATED")
+                .build();
+
+        try {
+            kafkaTemplate.send("patient", event.toByteArray());
+        } catch (Exception e) {
+            log.info("Error sending PatientCreated event: %s", e);
+        }
+    }
 }
